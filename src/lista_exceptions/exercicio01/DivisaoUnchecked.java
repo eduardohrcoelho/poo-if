3bf5aca088c4083f537f74cade09b46aa55ab27a
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-12-16 10:30:07 -0300

    Exercicio 01 Exceptions

---
+++ src/lista_exceptions/exercicio01/DivisaoUnchecked.java
@@ -0,0 +1,27 @@
+package lista_exceptions.exercicio01;
+import java.util.*;
+public class DivisaoUnchecked {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*
+        Sem tratamento:
+        System.out.print("Informe um número: ");
+        int a = scanner.nextInt();
+        System.out.print("Informe outro número: ");
+        int b = scanner.nextInt();
+        int divisao = a/b;
+        System.out.println("Resultado da divisão: " + divisao);
+        */
+
+        try {
+            System.out.print("Informe um número: ");
+            int a = scanner.nextInt();
+            System.out.print("Informe outro número: ");
+            int b = scanner.nextInt();
+            int divisao = a / b;
+            System.out.println("Resultado da divisão: " + divisao);
+        } catch (ArithmeticException e) {
+            System.out.println("Erro! Não é possível realizar divisão por zero");
+        }
+    }
+}
